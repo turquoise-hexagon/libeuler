bf6e22e4acc6b253dc9bb98cb766571518745989
turquoise-hexagon <camille@lzr.pw>
2023-10-22 14:52:02 +0100

    switch between fixnum and bignum on the fly in digits.scm

--- src/digits.scm
+++ src/digits.scm
@@ -2,12 +2,6 @@
 ;; functions
 ;; ---
 
-(define-inline (_digitsum n b)
-  (let loop ((n n) (acc 0))
-    (if (zero? n)
-      acc
-      (loop (quotient n b) (+ acc (modulo n b))))))
-
 (define-inline (_list->number l b)
   (let loop ((l l) (acc 0))
     (if (null? l)
@@ -17,13 +11,37 @@
           (error 'list->number "invalid value" i))
         (loop (cdr l) (+ (* acc b) i))))))
 
-(define-inline (_number->list n b)
+(define-inline (_number->list/fixnum n b)
+  (let loop ((n n) (acc '()))
+    (if (fx= n 0)
+      acc
+      (loop (fx/ n b) (cons (fxmod n b) acc)))))
+
+(define-inline (_number->list/bignum n b)
   (let loop ((n n) (acc '()))
     (if (zero? n)
       acc
       (loop (quotient n b) (cons (modulo n b) acc)))))
 
-(define-inline (_palindrome? n b)
+(define-inline (_digitsum/fixnum n b)
+  (let loop ((n n) (acc 0))
+    (if (fx= n 0)
+      acc
+      (loop (fx/ n b) (fx+ acc (fxmod n b))))))
+
+(define-inline (_digitsum/bignum n b)
+  (let loop ((n n) (acc 0))
+    (if (zero? n)
+      acc
+      (loop (quotient n b) (+ acc (modulo n b))))))
+
+(define-inline (_palindrome?/fixnun n b)
+  (let loop ((i n) (acc 0))
+    (if (fx= i 0)
+      (fx= n acc)
+      (loop (fx/ i b) (fx+ (fx* acc b) (fxmod i b))))))
+
+(define-inline (_palindrome?/bignum n b)
   (let loop ((i n) (acc 0))
     (if (zero? i)
       (= n acc)
@@ -32,13 +50,6 @@
 ;; ---
 ;; wrappers
 ;; ---
-
-(define (digitsum n #!optional (b 10))
-  (##sys#check-integer    n 'digitsum)
-  (##sys#check-integer    b 'digitsum)
-  (check-positive-integer n 'digitsum)
-  (check-positive-integer b 'digitsum)
-  (_digitsum n b))
 
 (define (list->number l #!optional (b 10))
   (##sys#check-list       l 'list->number)
@@ -51,11 +62,30 @@
   (##sys#check-integer    b 'number->list)
   (check-positive-integer n 'number->list)
   (check-positive-integer b 'number->list)
-  (_number->list n b))
+  ((if (and (fixnum? n)
+            (fixnum? b))
+     _number->list/fixnum
+     _number->list/bignum)
+   n b))
+
+(define (digitsum n #!optional (b 10))
+  (##sys#check-integer    n 'digitsum)
+  (##sys#check-integer    b 'digitsum)
+  (check-positive-integer n 'digitsum)
+  (check-positive-integer b 'digitsum)
+  ((if (and (fixnum? n)
+            (fixnum? b))
+     _digitsum/fixnum
+     _digitsum/bignum)
+   n b))
 
 (define (palindrome? n #!optional (b 10))
   (##sys#check-integer    n 'palindrome?)
   (##sys#check-integer    b 'palindrome?)
   (check-positive-integer n 'palindrome?)
   (check-positive-integer b 'palindrome?)
-  (_palindrome? n b))
+  ((if (and (fixnum? n)
+            (fixnum? b))
+     _palindrome?/fixnun
+     _palindrome?/bignum)
+   n b))
